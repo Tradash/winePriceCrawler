381106339dfcdbfd490c6a4bec4acbfd5fd419bd
abobkov <abobkov@synergy.ru>
2022-03-31 09:22:24 +0300

    Изменения: Добавлен 5-ти секундный интервал между запусками хромиума для избегания "waterfall" при старте сканирования нескольких категорий одновременно.

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
-import {startWorker} from './workers/initWorkers';
-import {db} from './db/dbController';
-import {shopData} from "./config";
+import { startWorker } from './workers/initWorkers';
+import { db } from './db/dbController';
+import { shopData } from './config';
 
 if (process.argv.length === 2) {
   console.log('Запущено приложение по парсингу цен');
   db.clearDb(0).then(() => {
     for (let i = 0; i < shopData.categories.length; i++) {
-      startWorker({
-        shopUrl: shopData.shopUrl,
-        categoriesUrl: shopData.categories[i],
-        descUrl: shopData.descUrl,
-      });
+      setTimeout(() => {
+        startWorker({
+          shopUrl: shopData.shopUrl,
+          categoriesUrl: shopData.categories[i],
+          descUrl: shopData.descUrl,
+        });
+      }, 5000 * i);
     }
     db.closeConnection();
   });
