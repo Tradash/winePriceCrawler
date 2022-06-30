29530f6a1202aea7b2f4306deab70333a2cc1041
abobkov <abobkov@synergy.ru>
2022-06-30 14:19:34 +0300

    Добавлен механизм разграничения доступных потоков обработки

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,13 @@
 export const isHeadless = false;
-export const maxRepeat = 5;
+export const maxRepeat = 5; //Количество повторных попыток, получения данных после получения ошибки
 export const shopData = {
   shopUrl: 'https://online.metro-cc.ru',
   descUrl: '?order=price_desc',
   categories: [
-    // '/category/alkogolnaya-produkciya/shampanskoe-igristye-vina',
-    // '/category/myasnye/myaso/govyadina',
-    '/category/chaj-kofe-kakao/kofe-zernovoj',
+    '/category/alkogolnaya-produkciya/shampanskoe-igristye-vina',
+    '/category/myasnye/myaso/govyadina',
+     '/category/chaj-kofe-kakao/kofe-zernovoj',
     '/category/alkogolnaya-produkciya/vino',
   ],
 };
+export const maxWorker = 2; // Количество максимальных параллельных потоков (более 2-х комп начинает плохо себя чувствовать)
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,73 @@
-import { startWorker } from './workers/initWorkers';
-import { db } from './db/dbController';
-import { shopData } from './config';
+import {startWorker} from './workers/initWorkers';
+import {db} from './db/dbController';
+import {maxRepeat, maxWorker, shopData} from './config';
+import {delay} from './utils';
+
+let countWorker = 0;
+
+interface IShopData {
+  categories: string;
+  isActive: boolean;
+  isReady: boolean;
+  repeatCounter: number;
+}
+
+const data: IShopData[] = shopData.categories.map((x) => {
+  return {
+    categories: x,
+    isActive: false,
+    isReady: false,
+    repeatCounter: 0,
+  };
+});
+
+const findNext2work = (data: IShopData[]): number => {
+  for (let i = 0; i < data.length; i++) {
+    if (!data[i].isActive && !data[i].isReady && data[i].repeatCounter < maxRepeat) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+const hasActive = (data: IShopData[]): boolean => {
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].isActive) {
+      return true;
+    }
+  }
+  return false;
+};
 
 if (process.argv.length === 2) {
   console.log('Запущено приложение по парсингу цен');
-  db.clearDb(0).then(() => {
-    for (let i = 0; i < shopData.categories.length; i++) {
-      setTimeout(() => {
-        startWorker({
-          shopUrl: shopData.shopUrl,
-          categoriesUrl: shopData.categories[i],
-          descUrl: shopData.descUrl,
-        });
-      }, 5000 * i);
+  db.clearDb(0).then(async () => {
+    let i = 0;
+    while (i != -1 || hasActive(data)) {
+      if (countWorker < maxWorker) {
+        i = findNext2work(data);
+        if (i != -1) {
+          countWorker++;
+          data[i].isActive = true;
+          data[i].repeatCounter++;
+          startWorker({
+            shopUrl: shopData.shopUrl,
+            categoriesUrl: shopData.categories[i],
+            descUrl: shopData.descUrl,
+          })
+            .then(() => {
+              countWorker--;
+              data[i].isActive = false;
+              data[i].isReady = true;
+            })
+            .catch(() => {
+              countWorker--;
+              data[i].isActive = false;
+              data[i].isReady = false;
+            });
+        }
+      }
+      await delay(5000);
     }
     db.closeConnection();
   });
